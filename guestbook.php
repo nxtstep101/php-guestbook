37e29a498d6fedded8093e9c4f241067b6785c38
nxtstep101 <nxtstep101@gmail.com>
2014-05-17 18:28:46 -0500

    Adding name
    Derp

--- guestbook.php
+++ guestbook.php
@@ -1,16 +1,18 @@
 <?php
 require "guestbook.html";
 if ($_POST) {
-  $comment = $_POST['comment'];
-  $handle  = fopen("guestbook.html", "a");
-  fwrite($handle, $comment);
-  fclose($handle);
+    $name    = $_POST['name'];
+    $comment = $_POST['comment'];
+    $handle  = fopen("guestbook.html", "a");
+    fwrite($handle, $name . "<br/>" . $comment);
+    fclose($handle);
 }
 ?>
 <!DOCTYPE html>
 <html>
    <body>
       <form name="form" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+         Name:<br /><input type="text" title="Enter your Username" name="name" /><br />
          Message:<br /><textarea name="comment" maxlength="1000" cols="25" rows="6"></textarea><br />
          <input type="submit" name="submit" value="submit">
       </form>
