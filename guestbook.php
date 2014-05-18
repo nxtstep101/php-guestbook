854583f1a32d5110df78f484afade5b66851582e
nxtstep101 <nxtstep101@gmail.com>
2014-05-17 22:24:08 -0500

    +space between comments and emboldened names

--- guestbook.php
+++ guestbook.php
@@ -1,14 +1,18 @@
 <?php
 require "guestbook.html";
 if ($_POST) {
-    $allowed_tags = '<b><i><br/>';
-    $stripName    = strip_tags($_POST['name'], $allowed_tags);
-    $stripComment = strip_tags($_POST['comment'], $allowed_tags);
-    $name         = $stripName;
-    $comment      = $stripComment;
-    $handle       = fopen("guestbook.html", "a");
-    fwrite($handle, $name . "<br/>" . $comment);
-    fclose($handle);
+    if (empty($_POST["name"]) || empty($_POST["comment"])) {
+        echo "<script type='text/javascript'>alert('Please fill out the form before submitting.');</script>";
+    } else {
+        $allowed_tags = '<b><i><br/>';
+        $stripName    = strip_tags($_POST['name'], $allowed_tags);
+        $stripComment = strip_tags($_POST['comment'], $allowed_tags);
+        $name         = $stripName;
+        $comment      = $stripComment;
+        $handle       = fopen("guestbook.html", "a");
+        fwrite($handle, "<b>" . $name . "</b>:<br/>" . $comment . "<br/><br/>");
+        fclose($handle);
+    }
 }
 ?>
 <!DOCTYPE html>
